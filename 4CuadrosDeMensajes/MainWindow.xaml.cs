4c816dcbf6c34ca2e8d0da8c05130a41b6d0a4d4
DAM2Alu23 <DAM2Alu23@B01-PC19>
2025-01-31 12:30:01 +0100

    nuevo

---
+++ 4CuadrosDeMensajes/MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _4CuadrosDeMensajes
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            while (true) //para un bucle eterno hasta que elige la opción que queremos, en este caso se rompe eligiendo "No"
+            {
+                MessageBoxResult result = MessageBox.Show("Sí o No", "Elige", MessageBoxButton.YesNo, MessageBoxImage.Question); //para guardar el resultado que haya seleccionado
+                if (result == MessageBoxResult.Yes)
+                {
+                    result = MessageBox.Show("Sí o No", "Elige", MessageBoxButton.YesNo, MessageBoxImage.Question);
+                }
+                else
+                {
+                    break;
+                }
+            }
+        }
+    }
+}
